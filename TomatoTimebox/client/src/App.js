ffe2e643493a2a744c40e6c5bb99dc5094f991ac
Zach McWhirter <zmac7811@gmail.com>
2020-10-30 14:07:59 -0500

    add a Note works

--- TomatoTimebox/client/src/App.js
+++ TomatoTimebox/client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { UserProfileProvider } from "./providers/UserProfileProvider";
 import { TaskProvider } from "./providers/TaskProvider";
-// import { NoteProvider } from "./providers/NoteProvider";
+import { NoteProvider } from "./providers/NoteProvider";
 import { CategoryProvider } from "./providers/CategoryProvider";
 import Header from "./components/Header";
 import ApplicationViews from "./components/ApplicationViews";
@@ -14,12 +14,12 @@ function App() {
     <Router>
       <UserProfileProvider>
         <TaskProvider>
-          {/* <NoteProvider> */}
-          <CategoryProvider>
-            <Header />
-            <ApplicationViews />
-          </CategoryProvider>
-          {/* </NoteProvider> */}
+          <NoteProvider>
+            <CategoryProvider>
+              <Header />
+              <ApplicationViews />
+            </CategoryProvider>
+          </NoteProvider>
         </TaskProvider>
       </UserProfileProvider>
     </Router >
---
+++ TomatoTimebox/client/src/components/Notes/NoteForm.js
@@ -0,0 +1,80 @@
+import React, { useContext, useState, useEffect } from "react";
+import { NoteContext } from "../../providers/NoteProvider";
+import { TaskContext } from "../../providers/TaskProvider";
+import { useHistory, Link, useParams } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+export default function NoteForm() {
+    const { addNote } = useContext(NoteContext);
+    const history = useHistory();
+    const { task, } = useContext(TaskContext);
+
+
+    const [note, setNote] = useState({
+        content: "",
+        createDateTime: "",
+        taskId: task.id
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleFieldChange = e => {
+        const stateToChange = { ...note };
+        stateToChange[e.target.id] = e.target.value;
+        setNote(stateToChange);
+    };
+
+    const createNewNote = e => {
+        e.preventDefault();
+        if (note.content === "") {
+            alert("You forgot to write your note!")
+        } else {
+            setIsLoading(true);
+        }
+        addNote(note)
+            .then((n) => {
+                history.push(`/tasks`)
+            })
+    };
+
+    return (
+        <>
+            <div className="form-container">
+                <Form className="newPostForm">
+                    <FormGroup className="newPost">
+                        <div className="input-container">
+                            <Label for="name">Content</Label>
+                            <Input
+                                type="text"
+                                required
+                                onChange={handleFieldChange}
+                                id="content"
+                                placeholder="Content"
+                                value={note.content}
+                            />
+                            <br />
+                            <Label for="createDateTime">Note Created On: </Label>
+                            <Input
+                                type="datetime-local"
+                                required
+                                onChange={handleFieldChange}
+                                id="createDateTime"
+                                placeholder="Note Created On:"
+                                value={note.createDateTime}
+                            />
+                            <div className="s">
+                                <Button
+                                    className="newPostSubmitButton"
+                                    type="submit"
+                                    disabled={isLoading}
+                                    onClick={createNewNote}
+                                >Submit</Button>
+                                <Link to={`/tasks`}><Button type="button" color="warning">Cancel</Button></Link>
+                            </div>
+                        </div>
+                    </FormGroup>
+                </Form>
+            </div>
+        </>
+    )
+}
